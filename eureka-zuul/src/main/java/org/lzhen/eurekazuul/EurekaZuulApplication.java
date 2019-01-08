34800293f02d68664c4bb563a9e5eb7f7633cc1b
liangjie <13408458876@163.com>
2019-01-08 11:45:08 +0800

    zuul

---
+++ eureka-zuul/src/main/java/org/lzhen/eurekazuul/EurekaZuulApplication.java
@@ -0,0 +1,16 @@
+package org.lzhen.eurekazuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class EurekaZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaZuulApplication.class, args);
+	}
+
+}
+
